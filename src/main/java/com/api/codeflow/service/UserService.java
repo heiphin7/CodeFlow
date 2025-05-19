e692b4ee8a24c25fb3c17d8415a976ed98e079f0
rshal <heiphin7and2@gmail.com>
2025-05-19 11:41:20 +0500

    fetch(auth): Implemented JwtRequestFilter with access & refresh tokens

--- src/main/java/com/api/codeflow/service/UserService.java
+++ src/main/java/com/api/codeflow/service/UserService.java
@@ -61,4 +61,8 @@ public class UserService {
 
         userRepository.save(user);
     }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
 }
